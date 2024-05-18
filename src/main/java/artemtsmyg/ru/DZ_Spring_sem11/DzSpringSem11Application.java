a15b7c626d92db04e72763516da2aea022164ff8
artemtsmyg <129445769+artemtsmyg@users.noreply.github.com>
2024-05-18 21:09:57 +0300

    First commit

---
+++ src/main/java/artemtsmyg/ru/DZ_Spring_sem11/DzSpringSem11Application.java
@@ -0,0 +1,15 @@
+package artemtsmyg.ru.DZ_Spring_sem11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DzSpringSem11Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DzSpringSem11Application.class, args);
+	}
+
+}
---
+++ src/test/java/artemtsmyg/ru/DZ_Spring_sem11/DzSpringSem11ApplicationTests.java
@@ -0,0 +1,13 @@
+package artemtsmyg.ru.DZ_Spring_sem11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzSpringSem11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
